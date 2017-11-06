856e879b49e18f065f35de2ba1461992551ee826
WittBulter <nanazuimeng123@gmail.com>
2017-11-07 00:44:39 +0800

    Utils: improve config

--- src/utils/config.default.ts
+++ src/utils/config.default.ts
@@ -1,5 +1,19 @@
 
-export default {
+export type Config = {
+  alias: any,
+  title: string,
+  minimap: boolean,
+  backToTop: boolean,
+  nofollow: boolean,
+}
+
+export type Catalog = {
+  fileName: string,
+  name: string,
+  children?: Catalog[],
+}
+
+export const defaultConfig: Config = {
   alias: {},
   title: 'Simpler Paper',
   minimap: false,
