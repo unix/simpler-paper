e8c38c7e96dcc9020f384578b3499a49af334b8b
WittBulter <nanazuimeng123@gmail.com>
2017-11-07 00:45:59 +0800

    Utils: update log

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -2,8 +2,11 @@ const chalk = require('chalk')
 
 export default {
   sourceError: (path?: string) => {
-    console.log(chalk.red(`\nError: the document directory was not found.`))
+    console.log(chalk.red('\nError: the document directory was not found.'))
     path && console.log(chalk.red(`PathError: "${path}".`))
-    console.log(chalk.green('try run [paper go -p {docuemnt_path}].\n'))
+    console.log(`try run [${chalk.green('paper build <docuemnt_path>')}].\n`)
+  },
+  configInvalid: () => {
+    console.log(chalk.red('\nError: paper.config.json is not a valid file.\n'))
   },
 }
