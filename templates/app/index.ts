9da955089e9723af6cff6be9c03a08f4de4a3c1f
WittBulter <nanazuimeng123@gmail.com>
2017-11-07 17:53:30 +0800

    App: create page

--- templates/app/index.ts
+++ templates/app/index.ts
@@ -1,2 +1,18 @@
-require('../temp/www.html')
-console.log(123)
+import { Catalog, Config } from '../../src/utils/config.default'
+
+const config: Config = require('../temp/paper.config.json')
+const catalogs: Catalog[] = require('../temp/catalogs.json')
+import * as Generator from './scripts/generator'
+
+const docker: Function = async(w: Window, d: Document): Promise<void> => {
+  const side: HTMLElement = d.getElementById('side')
+  
+  const list: HTMLElement = await Generator.side(catalogs, config)
+  side.appendChild(list)
+}
+
+window.onload = (): void => {
+  docker(window, document).then()
+}
+
+
