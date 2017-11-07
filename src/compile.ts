2b02c5c3b62b225c9dfc002b6f61c43802de7fc6
WittBulter <nanazuimeng123@gmail.com>
2017-11-07 15:41:22 +0800

    Compile: insert to app

--- src/compile.ts
+++ src/compile.ts
@@ -1,17 +1,28 @@
+import { resolve } from 'path'
 import File from './utils/file'
 import { Config, Catalog } from './utils/config.default'
 import * as marked from 'marked'
+const __temp = `${resolve()}/templates/temp/`
 
 
 const isMarkFileOrDirectory = (name: string): boolean => {
   return name.endsWith('.md') || !name.includes('.')
 }
 
-const generateCatalog = (name: string, config: Config, children: Catalog[] = []) => ({
-  fileName: name,
-  name: config.alias[name] ? config.alias[name] : name.split('.md')[0],
-  children,
-})
+const makeDirectoryName = (name: string): string => {
+  name = name.split('.md')[0]
+  if (!name.includes('/')) return name
+  return name.split('/').reverse()[0]
+}
+
+const generateCatalog = (name: string, config: Config, children: Catalog[] = []) => {
+  const directoryName: string = makeDirectoryName(name)
+  return {
+    fileName: name,
+    name: config.alias[directoryName] ? config.alias[directoryName] : directoryName,
+    children,
+  }
+}
 
 const generateDirectory = async(path: string, config: Config): Promise<Catalog[]> => {
   const files: string[] = await File.readdir(path)
@@ -34,7 +45,50 @@ const generateDirectory = async(path: string, config: Config): Promise<Catalog[]
 
 export const compileToHtml = async(path: string, config: Config) => {
   const catalogs: Catalog[] = await generateDirectory(path, config)
-  
-  console.log(catalogs)
+  return catalogs
+}
+
+const copyCatalogsFile = async(catalogs: Catalog[]): Promise<void> => {
+  await File.writeFile(`${__temp}/catalogs.json`, JSON.stringify(catalogs))
+}
+
+const copyConfigFile = async(config: Config): Promise<void> => {
+  await File.writeFile(`${__temp}/paper.config.json`, JSON.stringify(config))
+}
+
+const makeTargetPath = (path: string, sourcePath: string): string => {
+  return path.replace(sourcePath, __temp)
+}
+
+const createDirectory = async(targetPath: string): Promise<void> => {
+  if (!await File.exists(targetPath)) {
+    await File.mkdir(targetPath)
+  }
+}
+
+const createHtml = async(source: string, target: string): Promise<void> => {
+  const content: string = await File.readFile(source, 'utf-8')
+  const path: string = target.replace('.md', '.html')
+  await File.writeFile(path, marked(content), 'utf-8')
+}
+
+const generatePages = async(catalogs: Catalog[], sourcePath: string): Promise<void> => {
+  for (const unit of catalogs) {
+    const p: string = makeTargetPath(unit.fileName, sourcePath)
+    if (unit.children && unit.children.length > 0) {
+      await createDirectory(p)
+      await generatePages(unit.children, sourcePath)
+      continue
+    }
+    await createHtml(unit.fileName, p)
+  }
+}
+
+export const insertToApp = async (catalogs: Catalog[], sourcePath: string, config: Config) => {
+  File.spawnSync('rm', ['-rf', __temp])
+  await File.mkdir(__temp)
+  await generatePages(catalogs, sourcePath)
+  await copyConfigFile(config)
+  await copyCatalogsFile(catalogs)
 }
 
