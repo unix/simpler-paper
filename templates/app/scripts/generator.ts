e0abf69221430319c96703890e4d15f04fad9764
WittBulter <nanazuimeng123@gmail.com>
2017-11-08 16:55:33 +0800

    App: add event generator

--- templates/app/scripts/generator.ts
+++ templates/app/scripts/generator.ts
@@ -1,4 +1,5 @@
 import { Catalog, Config } from '../../../src/utils/config.default'
+import HTML = marked.Tokens.HTML
 
 const makeLink = (name: string, path: string): string => {
   return name.replace(path, '')
@@ -12,10 +13,13 @@ const makeList = async(catalogs: Catalog[], path, d: Document) => {
     
     // is directory
     if (unit.children && unit.children.length > 0) {
-      li.innerHTML = `<p><a>${unit.name}</a></p>`
-      li.appendChild(await makeList(unit.children, path, d))
+      li.innerHTML = `<p class="chapter dir"><a>${unit.name}</a></p>`
+      li.classList.add('sub-list-container')
+      const subList: HTMLElement = await makeList(unit.children, path, d)
+      subList.classList.add('sub-list')
+      li.appendChild(subList)
     } else {
-      li.innerHTML = `<p><a href="#${link}">${unit.name}</a></p>`
+      li.innerHTML = `<p class="chapter link"><a href="#${link}">${unit.name}</a></p>`
     }
     ul.appendChild(li)
   }
@@ -27,4 +31,26 @@ export const side = async(catalogs: Catalog[], config: Config): Promise<HTMLElem
   const d: Document = document
   const ul: HTMLElement = await makeList(catalogs, config.__user_source_path, d)
   return ul
+}
+
+
+const initSubList = async () => {
+  const containers: NodeListOf<Element> = document.querySelectorAll('.sub-list-container')
+  const subList: NodeListOf<Element> = document.querySelectorAll('.sub-list')
+  const handle: Function = (event: Event, container: Element) => {
+    const listLength = container.children.length - 1
+    const height: number = listLength * 40
+    const sub: HTMLElement = container.querySelector('.sub-list')
+    sub.style.height = sub.offsetHeight > 0 ? '0px' : `${height}px`
+  }
+  Array.from(containers).forEach(con => {
+    con.addEventListener('click', (event: Event) => handle(event, con))
+  })
+  Array.from(subList).forEach(sub => {
+    sub.addEventListener('click', (event: Event) => event.stopPropagation())
+  })
+}
+
+export const event = async(): Promise<void> => {
+  await initSubList()
 }
