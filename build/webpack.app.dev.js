99c95f630e4795127157673fc03f2f7c00f789d2
WittBulter <nanazuimeng123@gmail.com>
2017-11-11 23:01:16 +0800

    Build: remove extract-text-webpack-plugin

--- build/webpack.app.dev.js
+++ build/webpack.app.dev.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const webpackMerge = require('webpack-merge')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const baseConfig = require('./webpack.app')
 
 module.exports = webpackMerge(baseConfig, {
@@ -12,10 +11,6 @@ module.exports = webpackMerge(baseConfig, {
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
-  
-  plugins: [
-    new ExtractTextPlugin('style.css')
-  ],
   
   devServer: {
     historyApiFallback: true,
