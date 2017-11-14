403365d0e891366f4b21556e105c2a05275bbcb9
WittBulter <nanazuimeng123@gmail.com>
2017-11-14 23:28:14 +0800

    Generator: always init highlight

--- src/scripts/generator.ts
+++ src/scripts/generator.ts
@@ -104,9 +104,7 @@ export const event = async(config: Config, eventHub: any): Promise<void> => {
   await initSubList()
   
   config.backToTop && new BackToTop()
-  config.highlight && new Highlight(eventHub)
   config.indicator && new Indicator(eventHub)
-  
-  
+  new Highlight(eventHub)
   
 }
