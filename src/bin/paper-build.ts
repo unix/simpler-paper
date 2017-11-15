a805abf219dbd5e61ad5c6b05863fbbe848c9854
WittBulter <nanazuimeng123@gmail.com>
2017-11-15 19:10:00 +0800

    Bin: auto find source

--- src/bin/paper-build.ts
+++ src/bin/paper-build.ts
@@ -1,19 +1,14 @@
-import * as commander from 'commander'
-import { checkSource, checkConfig, checkTheme } from '../utils/check'
+import { checkSource, checkConfig, checkTheme, findSource, assignConfig } from '../utils/check'
 import { compileCatalog, compileMarkdown, copyTheme, copyInlineHtml } from '../compile'
 import { defaultConfig } from '../utils/config.default'
 import File from '../utils/file'
 import Log from '../utils/log'
+import chalk from 'chalk'
 import { resolve } from 'path'
 
 const removeDir = async(dir) => {
   File.exists(dir) && await File.exec(`rm -rf ${dir}`)
 }
-// parse path
-commander
-  .usage('<document-path> [document-path]')
-  .parse(process.argv)
-const sourcePath: string = `${commander.args[0]}`
 
 ;(async() => {
   const root = `${__dirname}/../..`
@@ -21,13 +16,22 @@ const sourcePath: string = `${commander.args[0]}`
   const templateTempPath = `${root}/templates/temp`
   
   // check path
-  if (!await checkSource(sourcePath)) return process.exit(1)
-  const config: Config = Object.assign({},
-    defaultConfig, await checkConfig(sourcePath), { __user_source_path: sourcePath },
-  )
+  Log.time.start('check config')
+  const source: string = await findSource(process.cwd())
+  if (!source) {
+    console.log(chalk.red('Error: not found paper.config.json'))
+    return Log.time.over(false)
+  }
+  if (!await checkSource(source)) return Log.time.over(false)
+  const config: Config = await assignConfig(source)
+  Log.time.over()
+  
+  
+  // make catalog and html fragment
   const targetPath = `${resolve()}/${config.output}`
   const catalogs: Catalog[] = await compileCatalog(config)
-  await compileMarkdown(catalogs, sourcePath)
+  await compileMarkdown(catalogs, source)
+  
   
   Log.time.start('generative theme')
   // reset target dir
