bf7637a4de8aef53ef744f1be3761477ec41c206
WittBulter <nanazuimeng123@gmail.com>
2017-11-15 17:46:59 +0800

    Bin: opt time log

--- src/bin/paper-deploy.ts
+++ src/bin/paper-deploy.ts
@@ -13,27 +13,27 @@ const message = commander.args[0] || 'paper update'
   const __target = `${__dirname}/../../templates/target`
   console.log(`deploy message: ${chalk.green(`${message}`)}`)
   if (!commander.args[0]) {
-    console.log(`you can use ${chalk.green('[-m]')} to add information.`)
+    console.log(`you can use ${chalk.green('[-m]')} to add information.\n`)
   }
   
-  Log.time.start()
+  Log.time.start('check in, clean up')
   if (!File.exists(__target)) {
     console.log(chalk.red('Error: not found document.'))
     console.log(chalk.green('you need to run the [paper build] first.'))
-    return process.exit(1)
+    return Log.time.over(false)
   }
   pages.clean()
-  Log.time.over('check, clean.')
+  Log.time.over()
   
-  Log.time.start()
+  Log.time.start('deploy to github')
   pages.publish(__target, {
     message,
     branch: 'gh-pages',
   }, err => {
     if (err) {
       console.log(chalk.red(`Error: ${err}`))
-      return process.exit(1)
+      return Log.time.over(false)
     }
-    Log.time.over('deploy success.')
+    Log.time.over()
   })
 })()
