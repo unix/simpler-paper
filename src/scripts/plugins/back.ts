d3930e75f3582979494f839d17f95558e75afac2
WittBulter <nanazuimeng123@gmail.com>
2017-11-29 12:06:36 +0800

    Back: rollback

--- src/scripts/plugins/back.ts
+++ src/scripts/plugins/back.ts
@@ -1,4 +1,3 @@
-import * as MoveTo from 'moveto'
 
 export class BackToTop {
   
@@ -6,6 +5,10 @@ export class BackToTop {
     btnElement.classList.toggle('show', show)
   }
   
+  static handleBackButton(container: HTMLElement): void {
+    container.scrollTop = 0
+  }
+
   constructor() {
     this.init()
   }
@@ -13,22 +16,22 @@ export class BackToTop {
   private makeBackBtn(): HTMLElement {
     const _div: HTMLElement = document.createElement('div')
     _div.classList.add('back-to-top')
-    _div.setAttribute('data-mt-duration', '300')
-    _div.setAttribute('data-target', '#')
     _div.innerHTML = `<div class="back-to-top-container"><span>Back</span></div>`
     return _div
   }
   
   private init(): void {
+    const container: HTMLElement = document.querySelector('#container-position')
     const main: HTMLElement = document.querySelector('.main')
     const backButton: HTMLElement = this.makeBackBtn()
     main.appendChild(backButton)
   
-    new MoveTo().registerTrigger(backButton)
-    document.addEventListener('scroll', () => {
-      BackToTop.toggleBackButton(backButton, document.scrollingElement.scrollTop > 300)
+    backButton.addEventListener('click', () => {
+      BackToTop.handleBackButton(container)
     })
-    
+    container.addEventListener('scroll', (e: Event) => {
+      BackToTop.toggleBackButton(backButton, (<Element>e.target).scrollTop > 300)
+    })
   }
 
 }
